8cd24958d7909865b495895fc65b53e63aade567
JanBur96 <jan.burghardt1706@icloud.com>
2020-07-19 15:31:42 +0200

    added another LS fix

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Declare variables
+// Store elements in variables
 const timeEL = document.getElementById('time')
 const addEL = document.getElementById('add-input');
 const taskListEL = document.getElementById('task-list');
@@ -7,6 +7,11 @@ const trashIconEL = document.querySelectorAll('fa-trash-alt')
 const iconEL = "far";
 const unchecked = "fa-circle";
 const checked = "fa-check-circle";
+
+getTime()
+setInterval(getTime, 1000)
+
+// Local Storage stuff (gets the items in LS and puts them back on the page)
 let itemsArray = localStorage.getItem('items')
   ? JSON.parse(localStorage.getItem('items'))
   : []
@@ -14,7 +19,68 @@ let itemsArray = localStorage.getItem('items')
 localStorage.setItem('items', JSON.stringify(itemsArray))
 const data = JSON.parse(localStorage.getItem('items'))
 
-const liCreator = (text) => {
+data.forEach((item) => {
+  liCreator(item)
+})
+
+
+//////////////////// EVENT LISTENERS ////////////////////
+// Add To-Do on click
+addButtonEL.addEventListener('click', () => {
+  if(addEL.value != '') {
+    addToList();
+}
+})
+
+
+// Add To-Do when pressing enter
+addEL.addEventListener('keyup', (e) => {
+  if(e.keyCode == 13 && addEL.value != '') {
+    addToList();
+  }
+})
+
+
+// Remove To-Do on click
+taskListEL.addEventListener('click', (e) => {
+  if(e.target.classList[1] == "fa-trash-alt" ) {
+    let targetedText = e.target.parentNode.childNodes[1].childNodes[0].data
+    console.log(targetedText)
+    let targetLocal = JSON.parse(localStorage.getItem('items'))
+    console.log(targetLocal)
+
+    for(let i = 0; i < data.length; i++) {
+      if(targetedText == targetLocal[i]) {
+        
+        targetLocal.splice(i, 1)
+        localStorage.clear()
+        localStorage.setItem("items", JSON.stringify(targetLocal))
+
+        data.splice(i, 1)
+        location.reload()
+      }
+      e.target.parentElement.remove()
+    }
+  }
+})
+
+// Mark To-Do as completed
+taskListEL.addEventListener('click', (e) => {
+  if(e.target.classList[1] == "fa-circle" ) {
+    e.target.classList.remove(unchecked)
+    e.target.classList.add(checked)
+    e.target.parentElement.childNodes[1].style.textDecorationLine = "line-through"
+  }
+  else if (e.target.classList[1] == "fa-check-circle") {
+    e.target.classList.add(unchecked)
+    e.target.classList.remove(checked)
+    e.target.parentElement.childNodes[1].style.textDecorationLine = "none"
+  }
+})
+
+//////////////////// FUNCTIONS ////////////////////
+// Add todo from LS to list
+function liCreator (text) {
   const newTask = document.createElement('li');
   taskListEL.appendChild(newTask)
 
@@ -42,12 +108,7 @@ const liCreator = (text) => {
   newTask.appendChild(arrowUp)
   newTask.appendChild(arrowDown)
   newTask.appendChild(newTaskTrash)
-
 }
-
-data.forEach((item) => {
-  liCreator(item)
-})
 
 // Get the current time
 function getTime () {
@@ -76,12 +137,8 @@ function getTime () {
   timeEL.textContent = currentTime;
 }
 
-getTime()
-setInterval(getTime, 1000);
-
-// Add To-Do on click
-addButtonEL.addEventListener('click', () => {
-  if(addEL.value != '') {
+// Add todo to the list
+function addToList () {
   const toDo = addEL.value;
 
   const newTask = document.createElement('li');
@@ -121,103 +178,65 @@ addButtonEL.addEventListener('click', () => {
 
   window.scrollTo(0, 0)
 }
-})
-
-// Add To-Do when pressing enter
-addEL.addEventListener('keyup', (e) => {
-  if(e.keyCode == 13 && addEL.value != '') {
-    const toDo = addEL.value;
-
-    const newTask = document.createElement('li');
-    taskListEL.appendChild(newTask)
-  
-    const newTaskCircle = document.createElement('i');
-    newTaskCircle.classList.add('far');
-    newTaskCircle.classList.add(unchecked);
-    
-    const newTaskText = document.createElement('p');
-    newTaskText.id = "toDoText";
-    newTaskText.textContent = toDo;
-
-    const arrowUp = document.createElement('i')
-    arrowUp.classList.add('fas')
-    arrowUp.classList.add('fa-arrow-up')
-  
-    const arrowDown = document.createElement('i')
-    arrowDown.classList.add('fas')
-    arrowDown.classList.add('fa-arrow-down')
-    
-    const newTaskTrash = document.createElement('i');
-    newTaskTrash.classList.add('far');
-    newTaskTrash.classList.add('fa-trash-alt');
-  
-    newTask.appendChild(newTaskCircle)
-    newTask.appendChild(newTaskText)
-    newTask.appendChild(arrowUp)
-    newTask.appendChild(arrowDown)
-    newTask.appendChild(newTaskTrash)
-    
-    addEL.value = '';
-
-    itemsArray.push(toDo)
-    localStorage.setItem('items', JSON.stringify(itemsArray))
-
-    location.reload()
-
-    window.scrollTo(0, 0)
-  }
-})
-
-// Remove To-Do on click
-taskListEL.addEventListener('click', (e) => {
-  if(e.target.classList[1] == "fa-trash-alt" ) {
-    let targetedText = e.target.parentNode.childNodes[1].childNodes[0].data
-    console.log(targetedText)
-    let targetLocal = JSON.parse(localStorage.getItem('items'))
-    console.log(targetLocal)
-
-    for(let i = 0; i < data.length; i++) {
-      if(targetedText == targetLocal[i]) {
-        
-        targetLocal.splice(i, 1)
-        localStorage.clear()
-        localStorage.setItem("items", JSON.stringify(targetLocal))
-
-        data.splice(i, 1)
-        location.reload()
-      } else {
-        
-      }
-      e.target.parentElement.remove()
-    }
-  }
-})
-
-
-// Mark To-Do as completed
-taskListEL.addEventListener('click', (e) => {
-  if(e.target.classList[1] == "fa-circle" ) {
-    e.target.classList.remove(unchecked)
-    e.target.classList.add(checked)
-    e.target.parentElement.childNodes[1].style.textDecorationLine = "line-through"
-  }
-  else if (e.target.classList[1] == "fa-check-circle") {
-    e.target.classList.add(unchecked)
-    e.target.classList.remove(checked)
-    e.target.parentElement.childNodes[1].style.textDecorationLine = "none"
-  }
-})
 
 // Move task up / down
 function moveUp(element) {
   if(element.previousElementSibling)
     element.parentNode.insertBefore(element, element.previousElementSibling);
+    
+    const from = itemsArray.indexOf(element.childNodes[1].textContent);
+    const to = itemsArray.indexOf(element.nextElementSibling.childNodes[1].textContent)
+
+    function moveItem(from, to) {
+      // remove `from` item and store it
+      var f = itemsArray.splice(from, 1)[0];
+      // insert stored item into position `to`
+      itemsArray.splice(to, 0, f);
+    }
+    
+    moveItem(from, to);
+
+    localStorage.removeItem('items')
+    
+    localStorage.setItem('items', JSON.stringify(itemsArray))
 }
+
+// Array.prototype.move = function(from, to) {
+//   this.splice(to, 0, this.splice(from, 1)[0]);
+// };
+
 function moveDown(element) {
   if(element.nextElementSibling)
     element.parentNode.insertBefore(element.nextElementSibling, element);
+    console.log(itemsArray)
+    console.log(element.childNodes[1].textContent)
+    console.log(itemsArray.indexOf(element.childNodes[1].textContent))
+    console.log(element.previousElementSibling.childNodes[1].textContent)
+    console.log(itemsArray.indexOf(element.previousElementSibling.childNodes[1].textContent))
+
+    const from = itemsArray.indexOf(element.childNodes[1].textContent);
+    const to = itemsArray.indexOf(element.previousElementSibling.childNodes[1].textContent)
+
+    function moveItem(from, to) {
+      // remove `from` item and store it
+      var f = itemsArray.splice(from, 1)[0];
+      // insert stored item into position `to`
+      itemsArray.splice(to, 0, f);
+    }
+    
+    moveItem(from, to);
+
+    localStorage.removeItem('items')
+    
+    localStorage.setItem('items', JSON.stringify(itemsArray))
 }
-document.querySelector('ul').addEventListener('click', function(e) {
+
+taskListEL.addEventListener('click', function(e) {
   if(e.target.className === 'fas fa-arrow-down') moveDown(e.target.parentNode);
   else if(e.target.className === 'fas fa-arrow-up') moveUp(e.target.parentNode);
 });
+
+
+// localStorage.removeItem('items')
+    
+//     localStorage.setItem('items', JSON.stringify(itemsArray))
